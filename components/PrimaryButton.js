770c79b151ff072ffe923948bc823955fe3b53eb
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-08-22 12:39:04 -0400

    created new components

---
+++ components/PrimaryButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({});
