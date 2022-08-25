eae934b55a1112107c78776b56847ddf42ab4320
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-08-25 09:40:34 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,46 @@
-import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
+import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
+import Colors from "./utils/colors";
 
 export default function App() {
-  return <StartGameScreen />;
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber} />;
+  }
+
+  return (
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}
+    >
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode={"cover"}
+        imageStyle={styles.backgroundImage}
+        style={styles.rootScreen}
+      >
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
