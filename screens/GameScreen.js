c4b789ab4d218c3c4ee24944f8110440d8cde9f5
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-08-25 09:38:40 -0400

    created GameScreen and StartGameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Title from "../components/Title/Title";
 
-const GameScreen = () => {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState();
+
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <View>
+        <Title>Opponent's Guess</Title>
+        <Text></Text>
+      </View>
+      <View></View>
+      <View>{/* log rounds */}</View>
     </View>
   );
 };
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,17 +1,92 @@
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
-import React from "react";
-import PrimaryButton from "../components/PrimaryButton";
+import React, { useState } from "react";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../utils/colors";
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onPickNumber }) => {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid Number",
+        "Number has to be a number between 1 and 99",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: resetInputHandler,
+          },
+        ]
+      );
+      return;
+    }
+    console.log(enteredNumber, "is a valid number");
+    onPickNumber(chosenNumber);
+  }
   return (
-    <View>
-      {/* <TextInput /> */}
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: Colors.accent500,
+    borderBottomWidth: 2,
+    color: Colors.accent500,
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
