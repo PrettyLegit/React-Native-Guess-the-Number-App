3e097242a76a5951a657a61baff865cb5a65520a
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-08-25 14:09:53 -0400

    Added styles

--- src/components/game/NumberContainer.js
+++ src/components/game/NumberContainer.js
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import Colors from "@/utils/colors";
 const NumberContainer = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
     </View>
   );
 };
 
 export default NumberContainer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    color: Colors.accent500,
+    padding: 24,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+});
